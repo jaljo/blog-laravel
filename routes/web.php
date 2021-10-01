76cd27fc0911a8899801bd18e6e63f039a8bfeef
Joris Langlois <joris.langlois@knplabs.com>
2021-10-01 11:46:02 +0300

    routing and wildcares

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/posts", function () {
+  return view("posts");
 });
+
+Route::get("/posts/{slug}", function (String $slug) {
+  try {
+    $post = file_get_contents(__DIR__ . "./../resources/posts/{$slug}.html");
+  } catch(Exception $e) {
+    abort(404, $e);
+  }
+
+  return view("post", [
+    "post" => $post,
+  ]);
+})
+  ->where("slug", "[\w\-_]+")
+;
